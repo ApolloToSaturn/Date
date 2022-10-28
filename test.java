2bac259b30e469aac1e3b653fb0e838fc6fe679e
ApolloToSaturn <114142672+ApolloToSaturn@users.noreply.github.com>
2022-10-28 13:05:27 +0200

    Add files via upload

---
+++ test.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class test {
+	public static void main(String [] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int j = scan.nextInt();
+		int m = scan.nextInt();
+		int t = scan.nextInt();
+		int s = scan.nextInt();
+		int m1 = scan.nextInt();
+		int m2 = scan.nextInt();
+		boolean langerMonat = (m == 1) || (m == 3) || (m == 5) || (m == 7) || (m == 8) || (m == 10) || (m == 12);
+		boolean kurzerMonat = (m == 4) || (m == 6) || (m == 9) || (m == 11);
+		boolean februar = (m == 2) && (t > 28);
+		boolean years = (j >= 1600) && (j <= 3000);
+		boolean months = (m >= 1) && (m <= 12);
+		boolean days = (t >= 1) && (t <= 31);
+		boolean hours = (s >= 0) && (s <= 23);
+		boolean minminutes = (m1 >= 0) && (m1 <= 59);
+		boolean plusminutes = (m2 >= 0) && (m2 <= 40000);
+		
+		if ((years) && (months) && (days) && (hours) && (minminutes) && (plusminutes)) { // Prüfen ob das Jahr im Wertebereich liegt
+			if ((februar) || ((kurzerMonat) && (t > 30)) || ((langerMonat) && (t > 31))) { // Prüfen ob angegebene Tage zu den richtigen Monaten passt
+						System.out.println("Der Monat " + m + " passt nicht zu den angegebenen Tagen " + t);
+			} else { for (int i=1; i <= m2; i++) { // for-Schleife die so oft läuft, wie m2 (Minuten die dazu addiert werden) läuft 
+				if (m1 + 1 > 59) {
+					m1 = 0;			
+			     	s = s + 1;
+			     	if (s > 23) { // Prüfen ob die Stunden größer 23 sind, wenn ja dann s auf 0 setzen und den Tag + 1 rechnen
+						 s = 0;
+						 t = t + 1;
+						 if ((februar) && (t > 28)) { // Prüfen ob der Monat Februar ist und mehr als 28 Tage hat, dann Tag + 1 und Monat + 1 rechnen
+							 t = 1;
+							 m = m + 1;
+						} else { if ((kurzerMonat) && (t > 30)) { // Prüfen ob der Monat zu den Monaten mit 30 Tagen zählt und ob dieser mehr als 30 Tage hat 
+									m = m + 1;
+								} else { if ((langerMonat) && (t > 31)) {  // Prüfen ob der Monat zu den Monaten mit 31 Tagen zählt und ob dieser mehr als 31 Tage hat 
+									t = 1;
+									m = m + 1;
+								} if (m > 12) { // wenn der Monat größer 12 ist, dann Tag auf 1, Monat auf 1 und das Jahr + 1 rechnen,
+										t = 1;
+										m = 1;
+										j = j + 1;
+								}
+								}
+						}
+					} 
+				}else { // Falls die Minuten nicht über 59 kommen, einfach + 1 rechnen
+					m1 = m1 + 1;
+				} 
+			}
+			}
+		} else {
+			System.out.println("Einer der Werte ist außerhalb des Wertebereichs"
+					+ "\nDie Wertebereiche sind:"
+					+ "\nJahr: >= 1600 und <= 3000"
+					+ "\nMonat: >= 1 und <= 12"
+					+ "\nTag: >= 1 und <= 31 - Ausnahmen sind hier zudem noch die unterschiedlichen Längen der einzelnen Monate"
+					+ "\nStunden: >= 0 und <= 23"
+					+ "\nMinuten: >= 0 und <= 59"
+					+ "\nMinuten plus: >= 0 und <= 40000"
+					+ "\nBitte Eingabe korrigieren.");
+		}
+		System.out.println(j + " " + m + " " + t + " " + s + " " + m1 + "\n");	
+		scan.close();
+	}	
+}
