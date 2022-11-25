e5004ef02369ecd01021877630b8c42be1da3bec
ApolloToSaturn <114142672+ApolloToSaturn@users.noreply.github.com>
2022-11-25 10:49:56 +0100

    Add files via upload

--- Vertauschen.java
+++ Vertauschen.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
+
 public class Vertauschen {
 	public static void main (String[] args) {
-		
-		
-		String[] test = new String[] {"ich", "habe", "kohldampf"};
-		
-		String[] inhalt = vertauscheInhalt(test);
-		String[] referenz = vertauscheReferenz(test);
+				
+		String[] inhalt = vertauscheInhalt(args);
+		String[] referenz = vertauscheReferenz(args);
 		
 		System.out.println(Arrays.toString(referenz));
 		System.out.println(Arrays.toString(inhalt));
 		System.out.println(Arrays.toString(vertauscheInhalt(referenz)));
 		System.out.println(Arrays.toString(vertauscheReferenz(referenz)));
+		System.out.println(referenz);
+		System.out.println(inhalt);
 		
 		if(!testInhaltGleich(inhalt, referenz)) {
 			System.out.println("Fehler: Inhalte stimmen nicht überein !");
@@ -23,14 +23,21 @@ public class Vertauschen {
 		
 	}
 	public static String[] vertauscheInhalt(String[] args) {
-		String tmp = "";
 		String[] newInhalt = new String[args.length];
 		for(int i=0; i < args.length;i++) {
+			String tmp = "";
 			tmp = args[args.length - 1 - i];
 			newInhalt[i] = tmp;
 		}
 		return newInhalt;
 	}
+	/*public static String[] vertauscheReferenz(String[] args) {
+		String[] newReferenz = new String[args.length];
+		for(int i=0; i < args.length;i++) {
+			newReferenz[i] = args[args.length - 1 - i];
+		}
+		return newReferenz;
+	}*/
 	public static String[] vertauscheReferenz(String[] args) {
 		String[] newReferenz = new String[args.length];
 		for(int i=0; i < args.length;i++) {
@@ -51,9 +58,10 @@ public class Vertauschen {
 	}
 	public static boolean testReferenzGleich(String[] arg1, String[] arg2) {
 		for(int i=0; i < arg1.length;i++) {
-			if(arg1[i] == arg2[i]) 
-				return true;
-				}
-		return false;
+			if(arg1[i] != arg2[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
